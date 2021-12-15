c1f417981d4fbfee95af5a9b2aaed0548900b641
AdityaPh <adityaphatak234@gmail.com>
2021-12-14 18:40:49 -0800

    addingBasicUI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { Card, Carousel, Container } from 'react-bootstrap'
 
 function App() {
   return (
+    // Add any styling you want
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      
+
+      <Card border="primary">
+        <Card.Body>
+          <div>
+            SOCRATES
+          </div>
+          <div>
+              <hr size="8" width="90%" color="lightBlue" />
+              <h1 style={{ textAlign:"center" , paddingTop:"0.5em"}}>Add content</h1>
+              <hr size="8" width="90%" color="lightBlue" />
+
+              <hr size="8" width="90%" color="lightBlue" />
+              <h1 style={{ textAlign:"center" , paddingTop:"0.5em"}}>Add content</h1>
+              <hr size="8" width="90%" color="lightBlue" />
+
+              
+          </div>
+        </Card.Body>
+      </Card>
+
     </div>
+
   );
 }
 
